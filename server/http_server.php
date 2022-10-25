0271befec4a84606016fecef9a7897df74ba319c
william ning <1158885641@qq.com>
2022-10-25 23:41:09 +0800

    swoole(2) - 快速启动

---
+++ server/http_server.php
@@ -0,0 +1,16 @@
+<?php
+
+$http = new Swoole\Http\Server('0.0.0.0', 9501);
+
+$http->on('Request', function ($request, $response) {
+    if ($request->server['path_info'] == '/favicon.ico' || $request->server['request_uri'] == '/favicon.ico') {
+        $response->end();
+        return;
+    }
+    var_dump($request->get, $request->post);
+    $response->header('Content-Type', 'text/html; charset=utf-8');
+    $response->end('<h1>Hello Swoole. #' . rand(1000, 9999) . '</h1>');
+});
+
+$http->start();
+
