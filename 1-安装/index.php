38edc3238aeb13b959de57a68a8cf4c9b31466dd
william ning <1158885641@qq.com>
2022-10-26 13:46:18 +0800

    swoole(1) - 安装「编译安装」

---
+++ 1-安装/index.php
@@ -0,0 +1,31 @@
+<?php
+
+// https://wiki.swoole.com/#/environment
+// https://blog.csdn.net/william_n/article/details/103868761
+
+// ➜  learning_of_swoole git:(main) ✗ php --ri swoole
+
+// swoole
+
+// Swoole => enabled
+// Author => Swoole Team <team@swoole.com>
+// Version => 4.8.12
+// Built => Oct 25 2022 20:49:20
+// coroutine => enabled with boost asm context
+// kqueue => enabled
+// rwlock => enabled
+// openssl => OpenSSL 3.0.5 5 Jul 2022
+// dtls => enabled
+// http2 => enabled
+// pcre => enabled
+// zlib => 1.2.11
+// async_redis => enabled
+
+// Directive => Local Value => Master Value
+// swoole.enable_coroutine => On => On
+// swoole.enable_library => On => On
+// swoole.enable_preemptive_scheduler => Off => Off
+// swoole.display_errors => On => On
+// swoole.use_shortname => On => On
+// swoole.unixsock_buffer_size => 262144 => 262144
+// ➜  learning_of_swoole git:(main) ✗ 
