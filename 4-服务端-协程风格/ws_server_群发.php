77fd6e4eae7d39905811118cac2cffd9e7c99917
william ning <1158885641@qq.com>
2022-11-05 13:23:42 +0800

    测试-服务端协程风格-WebSocket 服务器

---
+++ 4-服务端-协程风格/ws_server_群发.php
@@ -0,0 +1,39 @@
+<?php
+
+use Swoole\Http\Request;
+use Swoole\Http\Response;
+use Swoole\WebSocket\CloseFrame;
+use Swoole\Coroutine\Http\Server;
+use function Swoole\Coroutine\run;
+
+run(function () {
+    $server = new Server('127.0.0.1', 9503, false);
+    $server->handle('/websocket', function (Request $request, Response $ws) {
+        $ws->upgrade();
+        global $wsObjects;
+        $objectId = spl_object_id($ws);
+        $wsObjects[$objectId] = $ws;
+        while (true) {
+            $frame = $ws->recv();
+            if ($frame === '') {
+                unset($wsObjects[$objectId]);
+                $ws->close();
+                break;
+            } else if ($frame === false) {
+                echo 'errorCode: ' . swoole_last_error() . "\n";
+                $ws->close();
+                break;
+            } else {
+                if ($frame->data == 'close' || get_class($frame) === CloseFrame::class) {
+                    unset($wsObjects[$objectId]);
+                    $ws->close();
+                    break;
+                }
+                foreach ($wsObjects as $obj) {
+                    $obj->push("Server：{$frame->data}");
+                }
+            }
+        }
+    });
+    $server->start();
+});
